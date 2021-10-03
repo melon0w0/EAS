e9d3dea19d0e2b541edef891d6b6a7177d9e8f6f
Hanxi <yanghanxi980525@163.com>
2021-10-03 19:49:47 +0800

    most done except for the buttons for toggling on/off

--- script.js
+++ script.js
@@ -1,43 +1,76 @@
-// ctrl key || right click erase
-// alt key || left click paint
-// also provide toggle option
 let color = "#A58888";
 
 const inputBrushColor = document.querySelector(".inputBrushColor");
 const inputBackgroundColor = document.querySelector(".inputBackgroundColor");
-const canvas = document.querySelector(".canvas");
-const inputGridsize = document.querySelector(".inputGridsize");
-const displayGridsize = document.querySelectorAll(".displayGridsize");
-const buttonClear = document.querySelector(".clear");
-const buttonAddToPalette = document.querySelector(".addToPalette");
+
+const buttonAddToPalette = document.querySelector("button[data-index = '1'");
 const palette = document.querySelector(".palette");
 const customColor = document.querySelector(".customColor");
+const buttonResetPalette = document.querySelector("button[data-index = '2'");
 customColor.parentElement.removeChild(customColor);
+
+const inputGridsize = document.querySelector(".inputGridsize");
+const displayGridsize = document.querySelectorAll(".displayGridsize");
+
+const buttonClear = document.querySelector(".clear");
+const buttonToggleGrid = document.querySelector("button[data-index = '7']");
+
+const canvas = document.querySelector(".canvas");
 canvas.addEventListener("contextmenu", e => e.preventDefault());
+palette.addEventListener("contextmenu", e => e.preventDefault());
+
 
 //initialize the app
 resetGridsize(32);
 resetPalette();
 //////////////////////
 
+// colorInputElement.value  <= always in hex, and only accepts hex value;
+// element.style.backgroundColor <= accepts hex but always converts into rgb. like WHY?????
+
+// use element.innerHTML = "" to reset canvas/palette may not be the best practice
+// see https://stackoverflow.com/questions/3955229/remove-all-child-elements-of-a-dom-node-in-javascript
+
 inputBrushColor.addEventListener("input", function(e) {
-    color = e.target.value;
+    color = e.target.value; //hex
 })
 
 inputBackgroundColor.addEventListener("input", function(e) {
     for (let unit of canvas.children) {
-        unit.style.backgroundColor = e.target.value;
+        unit.style.backgroundColor = e.target.value; //rgb
     }
 })
+
+palette.addEventListener("click", function(e){
+    if(e.target.parentElement == palette && e.target.style.backgroundColor != "") {
+        color = e.target.style.backgroundColor; //rgb
+        //need to first convert color(in rgb) to hex
+        inputBrushColor.value = rgbToHex(color);
+    }
+})
+
+palette.addEventListener("contextmenu", function(e) {
+    if (e.target.parentElement == palette) {
+        e.target.style.backgroundColor = "";
+    }
+});
 
 buttonAddToPalette.addEventListener("click", function(e) {
     for (let child of palette.children) {
         if (child.style.backgroundColor == "") {
-            child.style.backgroundColor = color;
+            child.style.backgroundColor = color; //hex
             break;
         }
     }  
 })
+
+buttonResetPalette.addEventListener("click", resetPalette);
+function resetPalette() {
+    removeChildElements(palette);
+    for (let x = 0; x<20; x++) {
+        palette.appendChild(customColor.cloneNode());
+    }
+}
 
 inputGridsize.addEventListener("input", function(e) {
     let gridsize = e.target.value;
@@ -47,33 +80,49 @@ inputGridsize.addEventListener("input", function(e) {
 })
 
 function resetGridsize(gridsize) {
-    canvas.innerHTML = "";
+    removeChildElements(canvas);
     // populate canvas with grid units
     for (let x = 0; x < gridsize**2; x++) {
         let unit = document.createElement("div");
         unit.classList.add("unit");
+        if (buttonToggleGrid.value == "on") {
+            unit.classList.add("gridOn");
+        }
         unit.style.width = `${1/gridsize*100}%`;
         unit.style.height = `${1/gridsize*100}%`;
-
-        unit.addEventListener("mouseenter",changeColor);
         canvas.appendChild(unit);
     }
 }
 
-
-buttonClear.addEventListener("click", () => {
-    for (unit of canvas.children) {
-        unit.style.backgroundColor = "white";
+// buttons toggling section
+buttonToggleGrid.addEventListener("click", function(e) {
+    if (buttonToggleGrid.value == "off") {
+        buttonToggleGrid.value = "on";
+        buttonToggleGrid.textContent = "grid lines: on";
+        for (let unit of canvas.children) {
+            unit.classList.add("gridOn");
+        }
+    } else {
+        buttonToggleGrid.value = "off";
+        buttonToggleGrid.textContent = "grid lines: off";
+        for (let unit of canvas.children) {
+            unit.classList.remove("gridOn");
+        }
     }
-    inputBackgroundColor.value = "#FFFFFF";
-});
+})
 
+
+
+canvas.addEventListener("mousemove",changeColor); 
 function changeColor(e) {
-    if (checkPaintingMode(e) == "painting"){
-        e.target.style.backgroundColor = color;}
-    else if (checkPaintingMode(e) == "erasing"){
-        e.target.style.backgroundColor = "white";}
+    if (e.target.parentElement == canvas) {
+        if (checkPaintingMode(e) == "painting"){
+            e.target.style.backgroundColor = color;}
+        else if (checkPaintingMode(e) == "erasing"){
+            e.target.style.backgroundColor = "white";}
+    }
 }
+
 
 function checkPaintingMode(e) {
     if (e.altKey == true || e.buttons == 1) {
@@ -84,14 +133,34 @@ function checkPaintingMode(e) {
     }
 }
 
-function resetPalette() {
-    for (let x = 0; x<20; x++) {
-        palette.appendChild(customColor.cloneNode());
+buttonClear.addEventListener("click", () => {
+    for (unit of canvas.children) {
+        unit.style.backgroundColor = "white";
+    }
+    inputBackgroundColor.value = "#FFFFFF";
+});
+
+
+function rgbToHex(rgbString) {
+    let rgbArray = rgbString.match(/\d+/g);
+    let r = Number(rgbArray[0]);
+    let g = Number(rgbArray[1]);
+    let b = Number(rgbArray[2]);
+    return "#" + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
+  }
+
+
+function removeChildElements(parentElement) {
+    while (parentElement.firstChild) {
+        parentElement.removeChild(parentElement.lastChild);
     }
 }
+
 // e.buttons
 //1 left click
 //2 right click
 //3 both click
 
 // e.altKey, e.ctrlKey
+
+//rgb(x, x, x)    x can be 1-3 digits
